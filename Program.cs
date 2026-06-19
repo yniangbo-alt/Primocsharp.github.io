458c67055b1f8e49ebbfe318d64beb904f943b0a
Ylane Niangbo <yniangbo@gmail.com>
2026-06-20 01:21:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" GIOCCO DI LOTTERIA ");
+        const int NumMystere = 50;
+        bool vinto = false;  
+        while (vinto == false )
+        {
+            Console.Write("INSERISCE UN NUMERO TRA 1 & 100 : ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == NumMystere)
+            {
+                Console.WriteLine("Hai vinto");
+                vinto = true;
+            }
+            else if (num > NumMystere)
+            {
+                Console.WriteLine(" IL TUO NUMERO E PIU GRANDE ");
+            }
+            else if (num < NumMystere)
+            {
+                Console.WriteLine(" IL TUO NUMERO E PIU PICCOLO ");
+            }
+
+        }
+
+    }
+
+}
